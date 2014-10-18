de2bee503b2ec3f75c9a4efe53ad58018a064749
tachun <creed88@gmail.com>
2014-10-18 22:24:27 +0200

    change some image

--- front/app/game/GameState.js
+++ front/app/game/GameState.js
@@ -46,7 +46,7 @@ GameState.prototype.update = function() {
 
 // Load sprites
 GameState.prototype.preload = function() {
-  this.game.load.image('player', 'assets/player.jpg');
+  this.game.load.image('player', 'assets/player.png');
   // Load image map
   this.game.load.image('tiles', 'assets/tiles.png');
 };
