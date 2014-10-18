9a291b829969f14984af7905785ee850ff5eb231
Aymeric Beaumet <aymeric@beaumet.me>
2014-10-18 22:02:33 +0200

    Refactoring (optimization)

--- front/app/bridge/SocketPlayerManager.js
+++ front/app/bridge/SocketPlayerManager.js
@@ -3,46 +3,48 @@
 var id = null;
 
 var SocketPlayerManager = function SocketPlayerManager(){
-    this.x = 0;
-    this.y = 0;
+    this.position = {
+      x: 0,
+      y: 0
+    };
 
-    this.speedX = 0;
-    this.speedY = 0;
+    this.velocity = {
+      x: 0,
+      y: 0
+    };
 };
 
-SocketPlayerManager.prototype.position = function SocketPlayerManagerPosition(x, y){
-    this.x = x;
-    this.y = y;
+SocketPlayerManager.prototype.setPosition = function SocketPlayerManagerPosition(x, y){
+    this.position.x = x;
+    this.position.y = y;
 
     return this;
 };
 
-SocketPlayerManager.prototype.speed = function SocketPlayerManagerSpeed(x, y){
-    this.speedX = x;
-    this.speedY = y;
+SocketPlayerManager.prototype.setVelocity = function SocketPlayerManagerVelocity(x, y){
+    this.velocity.x = x;
+    this.velocity.y = y;
 
     return this;
 };
 
 SocketPlayerManager.prototype.build = function SocketPlayerManagerBuild(){
     var state = {
-        player : {
-            position : {
-                x : this.x,
-                y : this.y
-            },
-            speed : {
-                x : this.speedX,
-                y : this.speedY
-            }
-        }
+      p: {
+        x : this.position.x,
+        y : this.position.y
+      },
+      v: {
+        x : this.velocity.x,
+        y : this.velocity.y
+      }
     };
 
     return state;
 };
 
-SocketPlayerManager.prototype.setId = function SocketPlayerManagerSetId(nId){
-    id = nId;
+SocketPlayerManager.prototype.setId = function SocketPlayerManagerSetId(newId){
+    id = newId;
 };
 
 SocketPlayerManager.prototype.getId = function SocketPlayerManagerGetId(){
--- front/app/index.js
+++ front/app/index.js
@@ -1,9 +1,5 @@
-/**
- * Created by herzucco on 18/10/2014.
- */
 var SocketManager = require('./bridge/SocketsManager');
 window.s = SocketManager;
 
 // Require game.js
-//require('./game.js');
 require('./game/GameState.js');
