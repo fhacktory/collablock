2e25fe2dbd06fae2a82cecf99cd654759dfb00a9
Herzucco <a.carta@student.isartdigital.com>
2014-10-18 13:13:38 +0200

    start working with socket io

--- front/app/bridge/SocketsManager.js
+++ front/app/bridge/SocketsManager.js
@@ -1,3 +1,12 @@
 /**
  * Created by herzucco on 18/10/2014.
  */
+var socketIO = require('socket.io-client');
+var socket = socketIO();
+
+
+socket.on('connect', function(){
+    console.log('connected');
+    socket.on('event', function(data){});
+    socket.on('disconnect', function(){});
+});
