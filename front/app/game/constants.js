aba8459a4e6b5e4d086744860a2f55963ea7694d
Herzucco <a.carta@student.isartdigital.com>
2014-10-19 03:30:10 +0200

    some tweaking

--- front/app/game/constants.js
+++ front/app/game/constants.js
@@ -3,9 +3,9 @@
 module.exports = {
     MAX_VELOCITY: 500,
     ACCELERATION: 1500,
-    DRAG: 1000,
-    GRAVITY: 8000,
-    JUMP_VELOCITY: -1300,
+    DRAG: 10000,
+    GRAVITY: 4700,
+    JUMP_VELOCITY: -820,
     PLAYER_CUBE: {
       WIDTH: 32,
       HEIGHT: 32
